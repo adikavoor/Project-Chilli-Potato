257dcfb21a38b922f9ad8cbfabec220815dd0a1c
Shekhar Gulati <shekhargulati84@gmail.com>
2013-08-22 12:12:50 +0530

    themegaflasktutorial upto part 5

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='The Mega Flask Tutorial',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='The Mega Flask Tutorial Application',
+      author='Shekhar Gulati',
+      author_email='shekhargulati84@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
+	  install_requires=['flask' , 'flask-login' , 'flask-openid' , 'flask-email','sqlalchemy==0.7.9','flask-sqlalchemy' , 'sqlalchemy-migrate','flask-whooshalchemy','flask-wtf','flask-babel'],
      )
